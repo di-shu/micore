da105a62a9aa1e50367749b516ff3f4a67e1393e
DmitriyPchela <d.pchelyanskiy97z@gmail.com>
2020-09-04 16:07:55 +0300

    Fix build app

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const withPlugins = require('next-compose-plugins')
 const withTM = require('next-transpile-modules')(['gsap'])
 
 const nextConfig = {
+  distDir: 'build',
   devIndicators: {
     autoPrerender: false
   }
