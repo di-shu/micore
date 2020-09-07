fa0da3ee9b30fbb9902afba1f89bfd59f6edd835
DmitriyPchela <d.pchelyanskiy97z@gmail.com>
2020-09-07 12:54:31 +0300

    Fix links navigation

--- src/Components/SectionsComponents/ActiveLink.js
+++ src/Components/SectionsComponents/ActiveLink.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export const ActiveLink = ({ link, scroll = true, className, children, ...props }) => {
   return (
-    <Link href={link} scroll={scroll}>
+    <Link href={link} as={link} scroll={scroll}>
       <a className={className} {...props}>{children}</a>
     </Link>
   )
