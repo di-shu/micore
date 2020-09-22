2b29d8623d4a07d22457c6718075c1478dff8609
DmitriyPchela <d.pchelyanskiy97z@gmail.com>
2020-09-22 14:13:03 +0300

    Fix link routes

--- src/Components/Config/SkillsList.js
+++ src/Components/Config/SkillsList.js
@@ -9,31 +9,31 @@ export const SkillsList = [
         value: 'panel1',
         name: 'HTML5',
         icon: icon,
-        description: 'Очень хороший язык программирования, всем рекомендую. © Верстальщик Manticore'
+        description: ''
       },
       {
         value: 'panel2',
         name: 'css3',
         icon: icon,
-        description: 'Самый быстрый язык программирования. Лучше ООП решение. © Counter-Strike Source'
+        description: ''
       },
       {
         value: 'panel3',
         name: 'jquery',
         icon: icon,
-        description: 'Jquery говно, базарю. © Гоша Manticore'
+        description: 'J'
       },
       {
         value: 'panel4',
         name: 'canvas',
         icon: icon,
-        description: 'Наш сайт написан на нем. Очень крутая технология. Каждый наш разрабочтик знает ее. © Команда Manticore'
+        description: ''
       },
       {
         value: 'panel5',
         name: 'react',
         icon: icon,
-        description: 'React херня, я бы сделал лучше. © Вадим Дубровский'
+        description: ''
       }
     ]
   },
@@ -45,25 +45,25 @@ export const SkillsList = [
         value: 'panel1',
         name: 'PHP',
         icon: icon,
-        description: 'Технология для счетчика посетителей. © Расмус Лердорф'
+        description: ''
       },
       {
         value: 'panel2',
         name: 'LARAVEL',
         icon: icon,
-        description: 'Сука назвал фреймворк в честь жены, а она умерла. © Тейлор Отвел (разработчик)'
+        description: ''
       },
       {
         value: 'panel3',
         name: 'NODE.JS',
         icon: icon,
-        description: 'Почти как JS, но только сзади. © Охуенный шутник'
+        description: ''
       },
       {
         value: 'panel4',
         name: 'SYMPHONY',
         icon: icon,
-        description: 'Делал на symphony до того как это стало мэйнстримом. © Бетховен'
+        description: ''
       }
     ]
   },
@@ -75,7 +75,7 @@ export const SkillsList = [
         value: 'panel1',
         name: 'UI/UX',
         icon: icon,
-        description: 'Поиграйся со шрифтами. © Артем Лебедев'
+        description: ''
       }
     ]
   }
