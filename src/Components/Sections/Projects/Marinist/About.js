086d92df56749128fc3198c196d8ec8dd62a0736
DmitriyPchela <d.pchelyanskiy97z@gmail.com>
2020-10-26 12:15:03 +0200

    Fix projects site link

--- src/Components/Sections/Projects/Marinist/About.js
+++ src/Components/Sections/Projects/Marinist/About.js
@@ -6,7 +6,7 @@ export const About = () => {
   return (
     <ProjectAboutSection
       year="октябрь, 2019"
-      link="http://marinist.com.ua"
+      link="marinist.com.ua"
       name="Marinist Residence"
       description="МARINIST - строительный проект на одесском побережье крупного украинского холдинга. Наша задача в
       проекте состояла в разработке общего визуального стиля, логотипа и создании многофункционального сайта жилого дома."
